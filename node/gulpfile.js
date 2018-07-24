ff3826e422b7f6943664f7e08d333df0598b511e
Vladimir Kulik <bossonojka@gmail.com>
2018-07-24 20:58:32 +0300

    Babel-core was added

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var htmltidy = require('gulp-htmltidy');
 var autoprefixer = require('gulp-autoprefixer');
 var csslint = require('gulp-csslint');
+var jshint = require('gulp-jshint');
+var babel = require('gulp-babel');
 
-gulp.task('default', ['html', 'css']);
+gulp.task('default', ['html', 'css', 'js']);
 
 gulp.task('html', function(){
     return gulp.src('index.html')
@@ -18,6 +20,16 @@ gulp.task('css', function(){
     .pipe(autoprefixer({
         browsers: ['last 5 versions'],
         cascade: false
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('js', function(){
+    return gulp.src('main.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(babel({
+        presets: ['es2015']
     }))
     .pipe(gulp.dest('build'));
 });
