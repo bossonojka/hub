b92b93178cedac1a70cb641ee5b4fcc1bfe0f9d5
Vladimir Kulik <bossonojka@gmail.com>
2018-07-27 00:23:25 +0300

    Gulp task runner was configured for js transpiling

--- forGulp/build/main.js
+++ forGulp/build/main.js
@@ -1,16 +1,18 @@
+'use strict';
+
 var array = ['Chris', 'Bob', 'Mark', 'Paul'];
 
 // Arrow function syntax
-array.forEach((e, i, a) => {
-   var elem = document.createElement('p');
-   elem.textContent = (i + 1) + '. ' + e;
-   document.body.appendChild(elem);
+array.forEach(function (e, i, a) {
+    var elem = document.createElement('p');
+    elem.textContent = i + 1 + '. ' + e;
+    document.body.appendChild(elem);
 });
 
-fetch('style.css').then(response => {
-    return response.text().then((text) => {
-        let para = document.createElement("p");
+fetch('style.css').then(function (response) {
+    return response.text().then(function (text) {
+        var para = document.createElement("p");
         para.textContent = text;
         document.body.appendChild(para);
-    })
+    });
 });
--- forGulp/gulpfile.js
+++ forGulp/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
 var jshint = require("gulp-jshint");
+var babel = require("gulp-babel");
 
 gulp.task('default', ['js']);
 
@@ -7,5 +8,9 @@ gulp.task('js', function(){
     gulp.src('src/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
+    .pipe(babel())
+    .pipe(babel({
+        presets: ['babel-preset-es2015']
+    }))
     .pipe(gulp.dest('build'));
 });
