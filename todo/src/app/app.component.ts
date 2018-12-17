4b2585b614380a2587b354d8737c029a0f0e4a3d
Bossonojka <bossonojka@gmail.com>
2018-12-17 23:04:56 +0200

    Base functionality was added

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Model, ToDoItem } from "app/model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "todo-app",
+  templateUrl: "app.component.html"
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent{
+  model = new Model();
+
+  getName(){
+    return this.model.name;
+  }
+
+  getToDoItems(){
+    return this.model.items.filter(item => !item.done);
+  }
+
+  addItem(text){
+    if(text != ""){
+      this.model.items.push(new ToDoItem(text, false));
+    }
+  }
 }
