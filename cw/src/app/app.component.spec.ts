9914336e8ca068960ebfa5c6b598fbe4eff4cc0b
Bossonojka <bossonojka@gmail.com>
2019-01-08 22:14:04 +0200

    Name was changed

--- cw/src/app/app.component.spec.ts
+++ cw/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'untitled1'`, () => {
+  it(`should have as title 'cw'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('untitled1');
+    expect(app.title).toEqual('cw');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to untitled1!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cw!');
   });
 });
